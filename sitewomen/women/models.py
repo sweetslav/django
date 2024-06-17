d13d2a1832b016e5c29ba8d96ecef45b1b86774a
Svyatoslav Krivosheev <t1ksy@mail.ru>
2024-06-17 22:47:29 +0300

    day3

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -20,6 +20,7 @@ class Women(models.Model):
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts')
     tags = models.ManyToManyField('TagPost', blank=True, related_name='tags')
+    husband = models.OneToOneField('Husband', on_delete=models.SET_NULL, null=True, blank=True, related_name='wife')
 
     objects = models.Manager()
     published = PublishedManager()
@@ -57,3 +58,11 @@ class TagPost(models.Model):
 
     def get_absolute_url(self):
         return reverse('tag', kwargs={'tag_slug': self.slug})
+
+
+class Husband(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
