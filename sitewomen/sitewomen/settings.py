aa33e7c9a1c80f4b5873e03c7dd7a8153ce21233
Svyatoslav Krivosheev <t1ksy@mail.ru>
2024-06-27 14:42:12 +0300

    Добавил восстановление пароля через gmail

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -146,3 +146,14 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'users.authentication.EmailAuthBackend',
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'auriga678400@gmail.com'
+EMAIL_HOST_PASSWORD = 'xmjc crls xhck tgzv'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
